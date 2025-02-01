641e8bf410556282f4bb40fa6fec7b550183ccfc
sanoj1810 <sanojappu20@gmail.com>
2025-02-01 23:41:20 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div>
       <h1>Now,we start some changes in the file</h1>
+      <h2>
+        Hello, this is the second change.
+      </h2>
     </div>
   )
 }
